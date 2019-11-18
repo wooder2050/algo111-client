7fecd1d6321f8035bad35beaa87973fea2196d0f
Kang Jae-Young <wooder2050@gmail.com>
2019-11-18 14:21:07 +0900

    auth url modify

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,7 +6,7 @@ export default class Auth {
     domain: "jaeyoung.auth0.com",
     audience: "https://jaeyoung.auth0.com/userinfo",
     clientID: "ZyFfnFhkPTAmvjMIoUjtuJrMHNqc10qn",
-    redirectUri: "http://localhost:3000/callback",
+    redirectUri: "https://www.algo111.online/callback",
     responseType: "token id_token",
     scope: "openid profile"
   });
@@ -64,7 +64,7 @@ export default class Auth {
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
     this.auth0.logout({
-      returnTo: "http://localhost:3000",
+      returnTo: "https://www.algo111.online",
       clientID: "ZyFfnFhkPTAmvjMIoUjtuJrMHNqc10qn"
     });
   }
